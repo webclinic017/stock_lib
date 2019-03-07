4abe964c906887315ca48bab61f506ccec1a4c7f
fleth <fleth@users.noreply.github.com>
2019-03-07 09:22:49 +0900

    Revert "ranking add price"
    This reverts commit 046d294f4ebf7f37d4dfe898eca3279097b61619.

--- loader.py
+++ loader.py
@@ -766,7 +766,7 @@ class Loader:
         d = utils.format(date)
         try:
             data = pandas.read_csv("%s/%s/%s.csv" % (Loader.ranking_dir, ranking_type, d), header=None)
-            data.columns = ["code", "price"]
+            data.columns = ["code"]
             return data
         except:
             return None
