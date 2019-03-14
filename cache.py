62b995e630daac2f6c279441678dffb225c7066a
fleth <fleth@users.noreply.github.com>
2019-03-14 21:34:59 +0900

    add cache

---
+++ cache.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import os
+import pickle
+
+class Cache:
+    def __init__(self, cache_dir="/tmp"):
+        self.cache_dir = cache_dir
+
+    def dir(self):
+        return self.cache_dir
+
+    def path(self, name):
+        return "%s/%s.dump" % (self.dir(), name)
+
+    def exists(self, name):
+        return os.path.exists(self.path(name))
+
+    def create(self, name, data):
+        if not os.path.exists(self.dir()):
+            os.makedirs(self.dir())
+        with open(self.path(name), "wb") as f:
+            pickle.dump(data, f)
+
+    def get(self, name):
+        with open(self.path(name), "rb") as f:
+            cache = pickle.load(f)
+        return cache
+
+
