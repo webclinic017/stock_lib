20121e3f181334b1616974bde055b8da666e7e29
fleth <fleth@users.noreply.github.com>
2021-06-28 20:01:29 +0900

    update daily

--- strategies/ensemble.py
+++ strategies/ensemble.py
@@ -47,6 +47,11 @@ class CombinationStrategy(CombinationCreator):
 
     def common(self, setting):
         default = self.default_common()
+
+        default.new = [
+            lambda d: d.data.daily["rci"].iloc[-1] > d.data.daily["rci_long"].iloc[-1]
+        ]
+
         return default
 
     def get_size(self, size):
